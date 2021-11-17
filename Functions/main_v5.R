7acf3316b2c4c0f1c1619b3a5fb01c49ab14300e
Zitong Zhang <zztzhang@ucdavis.edu>
2021-11-16 21:52:57 -0800

    Change simulation setting

--- Functions/main_v5.R
+++ Functions/main_v5.R
@@ -18,9 +18,10 @@ main_v5 = function(### Parameters for generative model
   ### Parameters for algorithms
   freq_trun=15, bw=5, 
   conv_thres=1e-2, MaxIter=5,
-  jitter_time_rad = 0, max_iter=10,
+  jitter_time_rad = 10, max_iter=10,
   opt_radius=total_time/2,
   N_clus_min=N_clus-2, N_clus_max=N_clus+2,
+  freq_trun_vec=NULL,
   ...)
 {
   
@@ -41,73 +42,75 @@ main_v5 = function(### Parameters for generative model
   v_true_list = network_list$time_shift_list
   order_true_list = lapply(v_true_list, function(v_vec)order(v_vec))
   
-  
-  
-  
-  
 
   # Fit model for various cluster number ------------------------------------
   
   res_list = list()
-  for (N_clus_tmp in N_clus_min:N_clus_max) {
-    ### Get initialization -----------
-    res = get_init_v3(edge_time_mat_list = edge_time_mat_list, N_clus = N_clus_tmp, 
-                      v_true_list = v_true_list, 
-                      jitter_time_rad = jitter_time_rad,
-                      t_vec = t_vec)
+  if(is.null(freq_trun_vec)){
+    freq_trun_vec = c(freq_trun)
+  }
+  for (ind_N_clus in 1:length(N_clus_min:N_clus_max)) {
+    res_list[[ind_N_clus]] = list()
+    N_clus_tmp = c(N_clus_min:N_clus_max)[ind_N_clus]
     
-    clusters_list_init = res$clusters_list
-    n0_vec_list_init = res$n0_vec_list
-    n0_mat_list_init = n0_vec2mat(n0_vec = n0_vec_list_init)
-    
-    ### Evaluate accuracy of initial time shifts
-    spearman_corr_vec = numeric(length = N_subj)
-    for (m in 1:N_subj) {
-      spearman_corr_vec[m] = cor(v_true_list[[m]], n0_vec_list_init[[m]], method = "spearman")
+    for (ind_freq_trun in 1:length(freq_trun_vec)) {
+      freq_trun = freq_trun_vec[ind_freq_trun]
+      
+      ### Get initialization -----------
+      res = get_init_v3(edge_time_mat_list = edge_time_mat_list, N_clus = N_clus_tmp, 
+                        v_true_list = v_true_list, 
+                        jitter_time_rad = 0,
+                        t_vec = t_vec)
+      
+      clusters_list_init = res$clusters_list
+      n0_vec_list_init = res$n0_vec_list
+      n0_mat_list_init = n0_vec2mat(n0_vec = n0_vec_list_init)
+      
+      
+      # Apply algorithm ---------
+      
+      clusters_list_init -> clusters_list_est
+      n0_vec_list_init -> n0_vec_list_est
+      n0_mat_list_init -> n0_mat_list_est
+      
+      time_start = Sys.time()
+      ### Estimation z,v,f based on pdf
+      res = do_cluster_v14.2.1(edge_time_mat_list = edge_time_mat_list, N_clus = N_clus_tmp,
+                               clusters_list_init = clusters_list_est,
+                               n0_vec_list_init = n0_vec_list_est, n0_mat_list_init = n0_mat_list_est,
+                               total_time = total_time, max_iter=max_iter, t_vec=t_vec,
+                               freq_trun=freq_trun, 
+                               conv_thres=conv_thres, MaxIter=MaxIter,
+                               opt_radius=opt_radius,
+                               ...)
+      time_end = Sys.time()
+      time_estimation = time_end - time_start
+      
+      res$clusters_list -> clusters_list_est
+      res$v_vec_list -> v_vec_list_est
+      res$loss_history -> loss_history
+      res$align_time -> align_time
+      res$cluster_time -> cluster_time
+      res$center_pdf_array -> center_pdf_array_est
+      
+      N_iteration = res$N_iteration
+      
+      
+      ### Get estimated pdf using kernel smoothing
+      v_mat_list_est = n0_vec2mat(n0_vec = v_vec_list_est)
+      n0_mat_list_est = lapply(v_mat_list_est, function(v_mat)round(v_mat/(t_vec[2]-t_vec[1])))
+      center_pdf_array_est = get_center_pdf_array_v2(edge_time_mat_list = edge_time_mat_list,
+                                                     clusters_list = clusters_list_est,
+                                                     n0_mat_list = n0_mat_list_est,
+                                                     t_vec = t_vec)
+      res$center_pdf_array = center_pdf_array_est
+      
+      
+      # Save results of N_clus_tmp ----------------------------------------------
+      
+      res_list[[ind_N_clus]][[ind_freq_trun]] = res
+      
     }
-    spearman_corr_vec_mean_init = mean(spearman_corr_vec)
-    
-    
-    
-    # Apply algorithm ---------
-    
-    clusters_list_init -> clusters_list_est
-    n0_vec_list_init -> n0_vec_list_est
-    n0_mat_list_init -> n0_mat_list_est
-    
-    ### Estimation z,v,f based on pdf
-    res = do_cluster_v14.2.1(edge_time_mat_list = edge_time_mat_list, N_clus = N_clus_tmp,
-                             clusters_list_init = clusters_list_est,
-                             n0_vec_list_init = n0_vec_list_est, n0_mat_list_init = n0_mat_list_est,
-                             total_time = total_time, max_iter=max_iter, t_vec=t_vec,
-                             freq_trun=freq_trun, 
-                             conv_thres=conv_thres, MaxIter=MaxIter,
-                             opt_radius=opt_radius,
-                             ...)
-    
-    
-    res$clusters_list -> clusters_list_est
-    res$v_vec_list -> v_vec_list_est
-    res$loss_history -> loss_history
-    res$align_time -> align_time
-    res$cluster_time -> cluster_time
-    res$center_pdf_array -> center_pdf_array_est
-    
-    
-    ### Get estimated pdf using kernel smoothing
-    v_mat_list_est = n0_vec2mat(n0_vec = v_vec_list_est)
-    n0_mat_list_est = lapply(v_mat_list_est, function(v_mat)round(v_mat/(t_vec[2]-t_vec[1])))
-    center_pdf_array_est = get_center_pdf_array_v2(edge_time_mat_list = edge_time_mat_list,
-                                                   clusters_list = clusters_list_est,
-                                                   n0_mat_list = n0_mat_list_est,
-                                                   t_vec = t_vec)
-    res$center_pdf_array = center_pdf_array_est
-
-    
-    # Save results of N_clus_tmp ----------------------------------------------
-
-    res_list = c(res_list, list(res))
-    
   }
   
   
@@ -124,9 +127,15 @@ main_v5 = function(### Parameters for generative model
   N_clus_est = sel_mod_res$N_clus_est
   ICL_vec = sel_mod_res$ICL_vec 
   compl_log_lik_vec = sel_mod_res$compl_log_lik_vec 
-  penalty_vec = sel_mod_res$penalty_vec
+  log_lik_vec = sel_mod_res$log_lik_vec
   penalty_2_vec = sel_mod_res$penalty_2_vec
-    
+  penalty_vec = sel_mod_res$penalty_vec
+  ICL_mat = sel_mod_res$ICL_mat
+  compl_log_lik_mat = sel_mod_res$compl_log_lik_mat 
+  log_lik_mat = sel_mod_res$log_lik_mat
+  penalty_2_mat = sel_mod_res$penalty_2_mat
+  penalty_mat = sel_mod_res$penalty_mat
+  
   # Retrieve estimation results of the best cluster number ------------------
 
   res = sel_mod_res$res_best
@@ -283,8 +292,14 @@ main_v5 = function(### Parameters for generative model
               correct_N_clus=I(N_clus_est==N_clus)*1, 
               ICL_vec=ICL_vec, 
               compl_log_lik_vec=compl_log_lik_vec, 
-              penalty_vec=penalty_vec,
+              log_lik_vec=log_lik_vec,
               penalty_2_vec=penalty_2_vec,
+              penalty_vec=penalty_vec,
+              ICL_mat = ICL_mat,
+              compl_log_lik_mat = compl_log_lik_mat, 
+              log_lik_mat = log_lik_mat, 
+              penalty_2_mat = penalty_2_mat,
+              penalty_mat = penalty_mat,
               # parameter estimates of best cluster number
               clusters_list_est=clusters_list_est,
               v_vec_list_est=v_vec_list_est,
@@ -294,6 +309,8 @@ main_v5 = function(### Parameters for generative model
               F_mean_sq_err=F_mean_sq_err, 
               v_mean_sq_err=v_mean_sq_err,
               # other
+              time_estimation=time_estimation,
+              N_iteration=N_iteration,
               loss_history=loss_history, 
               cluster_time=cluster_time, 
               align_time=align_time
