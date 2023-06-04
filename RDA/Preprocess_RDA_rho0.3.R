47a12316443a89842bfd527f2340901a06fbabde
Zitong Zhang <zztzhang@ucdavis.edu>
2023-06-04 16:40:16 -0700

    SID-9 Save index of neurons to be analyzed in left / right spines (based on edge times obtained from rho=0.4)

--- RDA/Preprocess_RDA_rho0.3.R
+++ RDA/Preprocess_RDA_rho0.3.R
@@ -18,11 +18,11 @@ library(scales)
 
 
 # Basic connectivity inference --------------------------------------------
+# Input: folder_processed_data, subj_name_list, rho
+# Output: edge.time
 
-subj_name_list = c("func_20150417")
 folder_processed_data = '../Processed_FunctionalData/'
-
-
+subj_name_list = c("func_20150417")
 rho=0.3; 
 
 window_length = 240 # = 1min at 4 Hz
@@ -76,4 +76,43 @@ for(k in 1:length(subj_name_list)){
 }
 
 
+# Save index of neurons to be analyzed in left / right spines (based on edge times obtained from rho=0.4) ------------------------------------
+# Input: folder_processed_data, subj_name_list
+# Output: inds_neuron_analyzed_L, inds_neuron_analyzed_R
+
+folder_processed_data = "../Processed_FunctionalData/"
+subj_name_list = c("func_20150417")
+avai_inds_list = list()
+for(m in 1:length(subj_name_list)){ 
+  subj_name = subj_name_list[m]
+  
+  ### Read information from data
+  edge_time_mat = as.matrix(read.csv(paste(folder_processed_data,subj_name, '/EdgeTime.csv', sep='')))
+  edge_time_mat = edge_time_mat[,-1]
+  avai.inds = as.matrix(read.csv(paste(folder_processed_data,subj_name,'/AvaiNeurons.csv',sep='')))
+  avai.inds = avai.inds[,-1];
+  locs.all = as.matrix(read.csv(paste(folder_processed_data,subj_name,'/locs_all.csv',sep='')))
+  locs.all = locs.all[,-1]
+  locs_mat = locs.all[avai.inds,]
+  ### Separate networks in left and right spines
+  inds_L = which(locs_mat[,2]<0)
+  inds_R = which(locs_mat[,2]>0)
+  locs_mat_L = locs_mat[inds_L,]
+  locs_mat_R = locs_mat[inds_R,]
+  edge_time_mat_L = edge_time_mat[inds_L, inds_L]
+  edge_time_mat_R = edge_time_mat[inds_R, inds_R]
+  ### Remove neurons with no connection and neurons with abnormal behavior
+  avai_inds_L = which(rowSums(edge_time_mat_L<Inf)>0 )
+  avai_inds_R = which(rowSums(edge_time_mat_R<Inf)>0 )
+  if (TRUE | m==2) {
+    avai_inds_L = avai_inds_L[-c(1,2)]
+    avai_inds_R = avai_inds_R[-c(45,34,15)]
+  }
+  inds_neuron_analyzed_L = inds_L[avai_inds_L]
+  inds_neuron_analyzed_R = inds_R[avai_inds_R]
+
+  write.csv(inds_neuron_analyzed_L, paste(folder_processed_data,subj_name,'/Index_neuron_analyzed_L.csv',sep=''))
+  write.csv(inds_neuron_analyzed_R, paste(folder_processed_data,subj_name,'/Index_neuron_analyzed_R.csv',sep=''))
+  
+}
 
