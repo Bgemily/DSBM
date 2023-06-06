1a7e45b04e7813a8520dc33b3c049f4f8da7fc89
Zitong Zhang <zztzhang@ucdavis.edu>
2023-06-05 20:51:03 -0700

    SID-11 Reduce the separability of clusters. Increase the range of gamma. Use the same synthetic data for various gamma values.

--- sensitivity_gamma.R
+++ sensitivity_gamma.R
@@ -41,30 +41,27 @@ registerDoParallel(cores=N_cores)
 N_clus = 3
 
 ### ICL vs gamma, CDF, SAME scale across clusters -----
-if (FALSE){
+if (TRUE){
   ### Parameters' possible values:
-  ### n
-  N_node_persubj_list = list(30)
-  ### beta
-  conn_patt_sep_list = list(1.9)
   ### gamma
-  gamma_value_list = list(0.001,0.003,0.01,0.03,0.1,0.3,1)
+  gamma_value_list = list(0.001,0.003,0.01,0.03,0.1,0.3,1,3,10)
   
   top_level_folder = "../Results/Rdata"
   setup = 'sensitivity_gamma'
   method = 'main_v5_cdf_v1'
   
-  default_setting = 'pr=0.9,n=30,beta=1.9,V=80'
-  for (. in 1:split) {
+  default_setting = 'pr=0.9,n=30,beta=1.6,V=80'
+  for (id_split in 1:split) {
     ### gamma
     for (i in 1:length(gamma_value_list)) {
       gamma = gamma_value_list[[i]]
       results <- foreach(j = 1:N_trial) %dopar% {
-        SEED = sample(1:1e7,1)
+        SEED = id_split*10000+j
+        print(SEED)
         tryCatch(main_v5_cdf(SEED = SEED,
                              N_node_vec = rep(30,1),
                              conn_prob_mean = 0.9,
-                             conn_patt_sep = 1.9,
+                             conn_patt_sep = 1.6,
                              time_shift_mean_vec = rep(40,N_clus),
                              t_vec = seq(0,200,length.out=200),
                              gamma = gamma,
@@ -93,29 +90,25 @@ if (FALSE){
 ### ICL vs gamma, CDF, DIFFERENT scale across clusters -----
 
 ### Parameters' possible values:
-### n
-N_node_persubj_list = list(30)
-### beta
-conn_patt_sep_list = list(1.9)
 ### gamma
-gamma_value_list = list(0.001,0.003,0.01,0.03,0.1,0.3,1)
+gamma_value_list = list(0.001,0.003,0.01,0.03,0.1,0.3,1,3,10)
 
 top_level_folder = "../Results/Rdata"
 setup = 'sensitivity_gamma'
 method = 'main_v5_cdf_v1'
 
-default_setting = 'pr=vary,n=30,beta=1.9,V=80'
-for (. in 1:split) {
+default_setting = 'pr=vary,n=30,beta=1.6,V=80'
+for (id_split in 1:split) {
   ### gamma
   for (i in 1:length(gamma_value_list)) {
     gamma = gamma_value_list[[i]]
     results <- foreach(j = 1:N_trial) %dopar% {
-      SEED = sample(1:1e7,1)
+      SEED = id_split*10000+j
       tryCatch(main_v5_cdf(SEED = SEED,
                            N_node_vec = rep(30,1),
                            conn_prob_mean = 0.5,
                            conn_prob_rad = 0.4,
-                           conn_patt_sep = 1.9,
+                           conn_patt_sep = 1.6,
                            time_shift_mean_vec = rep(40,N_clus),
                            t_vec = seq(0,200,length.out=200),
                            gamma = gamma,
