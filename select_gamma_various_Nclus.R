fb85595a0b37323e7c70a7663e7379887c79f864
Zitong Zhang <zztzhang@ucdavis.edu>
2023-06-11 17:37:34 -0700

    SID-10 Add two scenarios when testing that the curvature of ICL vs gamma is consistent across N_clus_est.

--- select_gamma_various_Nclus.R
+++ select_gamma_various_Nclus.R
@@ -48,20 +48,55 @@ gamma_value_list = list(0.001,0.003,0.01,0.03,0.1,0.3,1,3,10)
 top_level_folder = "../Results/Rdata"
 setup = 'select_gamma_various_Nclus'
 
-
-
-default_setting = 'pr=0.9,n=30,beta=1.9,V=80'
-for (id_split in 1:split) {
+for (id_split in 1:split){
+  # ICL vs gamma, CDF, IDENTICAL scale across clusters -----
+  if (FALSE) {
+    default_setting = 'pr=0.9,n=30,beta=1.9,V=80'
+    for (N_clus_est in c(2,3,4)){
+      method = paste0('main_v5_cdf_Nclusest', N_clus_est)
+      for (i in 1:length(gamma_value_list)) {
+        gamma = gamma_value_list[[i]]
+        results <- foreach(j = 1:N_trial) %dopar% {
+          SEED = id_split*10000+j
+          print(SEED)
+          tryCatch(main_v5_cdf(SEED = SEED,
+                               N_node_vec = rep(30,1),
+                               conn_prob_mean = 0.9,
+                               conn_patt_sep = 1.9,
+                               time_shift_mean_vec = rep(40,N_clus),
+                               t_vec = seq(0,200,length.out=200),
+                               gamma = gamma,
+                               freq_trun_vec = c(Inf),
+                               MaxIter = 10,
+                               N_clus_min = N_clus_est,
+                               N_clus_max = N_clus_est),
+                   error = function(x) print(SEED))
+        }
+        param_name = "gamma"
+        param_value = gamma
+        folder_path = paste0(top_level_folder, '/', setup, '/', method,
+                             '/', default_setting,
+                             '/', param_name, '/', param_value)
+        dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+        
+        now_trial = format(Sys.time(), "%Y%m%d_%H%M%S")
+        save(results, file = paste0(folder_path, '/', 'N_trial', N_trial, '_', now_trial, '.Rdata'))
+        rm(results)
+      }
+    }
+  }
+  # ICL vs gamma, CDF, DIFFERENT scale and DIFFERENT shape across clusters -----
+  default_setting = 'pr=vary,n=30,beta=1.9,V=80'
   for (N_clus_est in c(2,3,4)){
     method = paste0('main_v5_cdf_Nclusest', N_clus_est)
     for (i in 1:length(gamma_value_list)) {
       gamma = gamma_value_list[[i]]
       results <- foreach(j = 1:N_trial) %dopar% {
         SEED = id_split*10000+j
-        print(SEED)
         tryCatch(main_v5_cdf(SEED = SEED,
                              N_node_vec = rep(30,1),
-                             conn_prob_mean = 0.9,
+                             conn_prob_mean = 0.5,
+                             conn_prob_rad = 0.4,
                              conn_patt_sep = 1.9,
                              time_shift_mean_vec = rep(40,N_clus),
                              t_vec = seq(0,200,length.out=200),
@@ -83,10 +118,41 @@ for (id_split in 1:split) {
       save(results, file = paste0(folder_path, '/', 'N_trial', N_trial, '_', now_trial, '.Rdata'))
       rm(results)
     }
-    
+  }
+  # ICL vs gamma, CDF, DIFFERENT scale and IDENTICAL shape across clusters -----
+  default_setting = 'pr=vary,n=30,beta=1,V=80'
+  for (N_clus_est in c(2,3,4)){
+    method = paste0('main_v5_cdf_Nclusest', N_clus_est)
+    for (i in 1:length(gamma_value_list)) {
+      gamma = gamma_value_list[[i]]
+      results <- foreach(j = 1:N_trial) %dopar% {
+        SEED = id_split*10000+j
+        tryCatch(main_v5_cdf(SEED = SEED,
+                             N_node_vec = rep(30,1),
+                             conn_prob_mean = 0.5,
+                             conn_prob_rad = 0.4,
+                             conn_patt_sep = 1,
+                             time_shift_mean_vec = rep(40,N_clus),
+                             t_vec = seq(0,200,length.out=200),
+                             gamma = gamma,
+                             freq_trun_vec = c(Inf),
+                             MaxIter = 10,
+                             N_clus_min = N_clus_est,
+                             N_clus_max = N_clus_est),
+                 error = function(x) print(SEED))
+      }
+      param_name = "gamma"
+      param_value = gamma
+      folder_path = paste0(top_level_folder, '/', setup, '/', method,
+                           '/', default_setting,
+                           '/', param_name, '/', param_value)
+      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+      
+      now_trial = format(Sys.time(), "%Y%m%d_%H%M%S")
+      save(results, file = paste0(folder_path, '/', 'N_trial', N_trial, '_', now_trial, '.Rdata'))
+      rm(results)
+    }
   }
   
 }
-
-
 
