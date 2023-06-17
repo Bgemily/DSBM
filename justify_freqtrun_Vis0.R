85020e0131d752eff833cee572dc446f0dcc8d02
Zitong Zhang <zztzhang@ucdavis.edu>
2023-06-17 02:10:31 -0700

    SID-10 Add more simulation replicates

--- justify_freqtrun_Vis0.R
+++ justify_freqtrun_Vis0.R
@@ -17,9 +17,9 @@ library(doParallel)
 # User input setup --------------------------------------------------------
 
 option_list = list(
-  make_option(c("-n", "--N_trial"), type="integer", default=200, 
+  make_option(c("-n", "--N_trial"), type="integer", default=1800, 
               help="number of repeated trials"),
-  make_option("--split", type="integer", default=20)
+  make_option("--split", type="integer", default=180)
 ); 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
@@ -52,7 +52,7 @@ for (id_split in 1:split) {
   method = paste0('main_v5_pdf')
   for (freq_trun in 2:9) {
     results <- foreach(j = 1:N_trial) %dopar% {
-      SEED = id_split*10000+j
+      SEED = id_split*100+j
       tryCatch(main_v5_pdf(SEED = SEED,
                            N_node_vec = rep(30,1),
                            conn_prob_mean = 0.9,
