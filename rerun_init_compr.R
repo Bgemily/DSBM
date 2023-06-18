a67c4a7a2692b1a8161de90393234fb6115e9191
Zitong Zhang <zztzhang@ucdavis.edu>
2023-06-18 14:20:18 -0700

    SID-10 Add simulation

--- rerun_init_compr.R
+++ rerun_init_compr.R
@@ -52,45 +52,48 @@ default_setting = 'pr=0.9,n=30,beta=1.9,V=80'
 method = 'main_v5_cdf_MISE_F'
 for (id_split in 1:split) {
   ### Random init scheme
-  for (N_restart in c(1,2,3,10)) {
+  if (FALSE) {
+    for (N_restart in c(1,2,3,10)) {
+      for (i in 1:length(conn_patt_sep_list)) {
+        conn_patt_sep = conn_patt_sep_list[[i]]
+        results <- foreach(j = 1:N_trial) %dopar% {
+          SEED = id_split*100+j+40000
+          tryCatch(main_v5_cdf(SEED = SEED,
+                               N_node_vec = rep(30,1),
+                               conn_prob_mean = 0.9,
+                               conn_patt_sep = 1.9,
+                               time_shift_mean_vec = rep(40,3),
+                               t_vec = seq(0,200,length.out=200),
+                               freq_trun_vec=c(Inf),
+                               gamma = 0.01,
+                               save_est_history = TRUE,
+                               rand_init=TRUE,
+                               N_restart = N_restart,
+                               conv_thres = 0, MaxIter = 10,
+                               N_clus_min = 3, N_clus_max = 3),
+                   error = function(x) print(SEED))
+        }
+        param_name = "beta"
+        param_value = conn_patt_sep
+        folder_path = paste0(top_level_folder, '/', setup, '/', method,
+                             '/', 'N_restart_v2.4', '/', N_restart,
+                             '/', default_setting,
+                             '/', param_name, '/', param_value)
+        dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
+        
+        now_trial = format(Sys.time(), "%Y%m%d_%H%M%S")
+        save(results, file = paste0(folder_path, '/', 'N_trial', N_trial, '_', now_trial, '.Rdata'))
+        rm(results)
+      }
+    }
+  }
+  
+  ### Proposed init scheme
+  if (TRUE) {
     for (i in 1:length(conn_patt_sep_list)) {
       conn_patt_sep = conn_patt_sep_list[[i]]
       results <- foreach(j = 1:N_trial) %dopar% {
         SEED = id_split*100+j+40000
-        tryCatch(main_v5_cdf(SEED = SEED,
-                             N_node_vec = rep(30,1),
-                             conn_prob_mean = 0.9,
-                             conn_patt_sep = 1.9,
-                             time_shift_mean_vec = rep(40,3),
-                             t_vec = seq(0,200,length.out=200),
-                             freq_trun_vec=c(Inf),
-                             gamma = 0.01,
-                             save_est_history = TRUE,
-                             rand_init=TRUE,
-                             N_restart = N_restart,
-                             conv_thres = 0, MaxIter = 10,
-                             N_clus_min = 3, N_clus_max = 3),
-                 error = function(x) print(SEED))
-      }
-      param_name = "beta"
-      param_value = conn_patt_sep
-      folder_path = paste0(top_level_folder, '/', setup, '/', method,
-                           '/', 'N_restart_v2.4', '/', N_restart,
-                           '/', default_setting,
-                           '/', param_name, '/', param_value)
-      dir.create(path = folder_path, recursive = TRUE, showWarnings = FALSE)
-      
-      now_trial = format(Sys.time(), "%Y%m%d_%H%M%S")
-      save(results, file = paste0(folder_path, '/', 'N_trial', N_trial, '_', now_trial, '.Rdata'))
-      rm(results)
-    }
-  }
-  ### Proposed init scheme
-  if (FALSE) {
-    for (i in 1:length(conn_patt_sep_list)) {
-      conn_patt_sep = conn_patt_sep_list[[i]]
-      results <- foreach(j = 1:N_trial) %dopar% {
-        SEED = id_split*100+j+20000
         tryCatch(main_v5_cdf(SEED = SEED,
                              N_node_vec = rep(30,1),
                              conn_prob_mean = 0.9,
